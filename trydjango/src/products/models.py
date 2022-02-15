1d8f0fb7353db11d40e763f83fbaacfcd6aef9dc
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-02-15 17:24:16 +0300

    django

--- trydjango/src/products/models.py
+++ trydjango/src/products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=120)  # max_length must be defined
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=False, null=True)
     price = models.DecimalField(max_digits=1000, decimal_places=2)
-    summary = models.TextField(default="How cool is this?")
+    summary = models.TextField(blank=False, null=False)
     featured = models.BooleanField(default=False)
