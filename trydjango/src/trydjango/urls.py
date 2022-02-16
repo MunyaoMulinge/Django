ece55cb14412d80f46ce99077d800783b12ef388
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-02-16 13:57:33 +0300

    django

--- trydjango/src/trydjango/urls.py
+++ trydjango/src/trydjango/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vic/', home_view, name='home'),
+    path('', home_view, name='home'),
+
 ]
