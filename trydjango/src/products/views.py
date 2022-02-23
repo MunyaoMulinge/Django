24a221f41964bd13f00fa75f97d3f2e86e7a0250
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-02-23 15:55:07 +0300

    raw html

--- trydjango/src/products/views.py
+++ trydjango/src/products/views.py
@@ -28,5 +28,10 @@ def product_detail_view(request):
 # }
 # return render(request, "products/product_create.html", context)
 def product_create_view(request):
+    #print(request.GET)
+    #print(request.POST)
+    if request.method == 'POST':
+        my_new_title = request.POST.get('title')
+        print(my_new_title)
     context = {}
     return render(request, "products/product_create.html", context)
