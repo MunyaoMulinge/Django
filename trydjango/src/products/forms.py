085041543d3c2ec0189f58dfa3ec16d0acd61bcc
MunyaoMulinge <munyaomulinge@protonmail.com>
2022-03-03 17:41:36 +0300

    pure django form

--- trydjango/src/products/forms.py
+++ trydjango/src/products/forms.py
@@ -4,6 +4,7 @@ from .models import Product
 
 class ProductForm(forms.ModelForm):
     title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    email = forms.EmailField()
     description = forms.CharField(required=False, widget=forms.Textarea(attrs={
         "placeholder": "Your description",
         "class": "new-class-name-two",
@@ -25,7 +26,16 @@ def clean_title(self, *args, **kwargs):
     title = self.cleaned_data.get("title")
     if not "CFE" in title:
         raise forms.ValidationError("Not a valid title")
+    if not "news" in title:
+        raise forms.ValidationError("Not a valid title")
     return title
+
+
+def clean_email(self, *args, **kwargs):
+    email = self.cleaned_data.get("email")
+    if not email.endswith("edu"):
+        raise forms.ValidationError("Invalid Email")
+    return email
 
 
 class RawProductForm(forms.Form):
